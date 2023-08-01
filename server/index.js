94d159e4828f747f7eddf137c521085ced0e6e40
David Russell <davidrussellrgb@gmail.com>
2023-08-01 16:28:02 -0500

    Init commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require('dotenv').config();
+
+const port = process.env.STATUS === 'production' ? process.env.PROD_PORT : process.env.DEV_PORT;
+
+const package = require('./package.json');
+
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+app.get('/api/version', (req, res) => {
+    return res.status(200).json({ version: package.version });
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../cards/build', 'index.html'));
+})
+
+app.listen(port, () =>  console.log(`Portfolio listening on port ${port}!`));
