1304a3403cb59d401227d1a304e4dc005113dd30
David Russell <davidrussellrgb@gmail.com>
2023-08-19 00:26:33 -0500

    Added skip animation button

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,15 +1,65 @@
+import { useEffect, useRef } from 'react';
+
 import EmailIcon from '@mui/icons-material/Email';
 import FilePresentIcon from '@mui/icons-material/FilePresent';
 import PhoneIcon from '@mui/icons-material/Phone';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
 
 import Resume from '../documents/Resume-David-Russell.pdf';
 
 import './Intro.css';
 
-export default function Intro({ hideIntro }) {
+export default function Intro({ animationComplete, setAnimationComplete, setIndex }) {
+    const classAddTimeout = useRef(null);
+    const completeAnimationTimeout = useRef(null);
+
+    useEffect(() => {
+        classAddTimeout.current = setTimeout(() => document.getElementById('skip-button').classList.add('fade-out'), 13000);
+        completeAnimationTimeout.current = setTimeout(() => setAnimationComplete(true), 13200);
+    }, []);
+
     return (
         <div className="intro">
-            <h1 className="fade first-fade">Hello!</h1>
+            <span className="top-span fade first-fade">
+                <h1>Hello!</h1>
+                {
+                    !animationComplete &&
+                    <button id="skip-button" title="Skip Animations" onClick={() => {
+                        const fadeElements = document.querySelectorAll('.fade');
+                        fadeElements.forEach((element) => {
+                            element.style.animationDelay = "0s";
+                        });
+
+                        const slideshodwFadeElements = document.querySelectorAll('.slideshow-fade');
+                        slideshodwFadeElements.forEach((element) => {
+                            element.style.animationDelay = "0s";
+                        });
+
+                        const sliderWrapperFadeElements = document.querySelectorAll('.slider-wrapper-fade');
+                        sliderWrapperFadeElements.forEach((element) => {
+                            element.style.animationDelay = "0s";
+                        });
+
+                        const slideshowButtonsFadeElements = document.querySelectorAll('.slideshow-buttons-fade');
+                        slideshowButtonsFadeElements.forEach((element) => {
+                            element.style.animationDelay = "0s";
+                        });
+
+                        const projectContentFadeElements = document.querySelectorAll('.project-content-fade');
+                        projectContentFadeElements.forEach((element) => {
+                            element.style.animationDelay = "0s";
+                        });
+
+                        document.getElementById('skip-button').classList.add('fade-out')
+                        setTimeout(() => setAnimationComplete(true), 200);
+                        clearTimeout(classAddTimeout.current);
+                        clearTimeout(completeAnimationTimeout.current);
+                        setIndex(0);
+                    }}>
+                        <SkipNextIcon fontSize="inherit" />
+                    </button>
+                }
+            </span>
             <div className="second-group">
                 <h2 className="fade second-fade">My name is David Russell</h2>
                 <h2 className="fade third-fade">I'm a software developer</h2>
